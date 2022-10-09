b8ec4133cdfdc54ea927de015377d41490b4bd20
Ricky <1372386862@qq.com>
2022-10-09 11:03:33 +0800

    项目预览

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
+  publicPath: './',
+  outputDir: path.resolve(__dirname, '../docs')
 })
