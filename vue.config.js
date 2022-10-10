b932b4e0fc5a9791dffb6b0c96ef2954a340ca3d
Ricky <1372386862@qq.com>
2022-10-10 09:53:36 +0800

    更新图标

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,14 @@ const path = require('path')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
-  outputDir: path.resolve(__dirname, './docs')
+  outputDir: path.resolve(__dirname, './docs'),
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  }
 })
